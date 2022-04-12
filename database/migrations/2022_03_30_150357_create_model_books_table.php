e14c73127a7bccffc8cff91b230fef9b873cff7e
Riosgui <guilhermerios100@hotmail.com>
2022-04-12 09:18:26 -0300

    Commit inicial

---
+++ database/migrations/2022_03_30_150357_create_model_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments(column:'id');
+            $table->integer(column:'id_user')->unsigned();
+            $table->foreign(columns:'id_user')->references(columns:'id')->on(table:'users')->onDelete(action:'cascade')->onUpdate(action:'cascade');
+            $table->string(column:'title');
+            $table->integer(column:'pages');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
